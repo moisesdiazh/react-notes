553126fb19ff6f6ec0b6c7ff159f74d434c7e62b
moisesdiazh <79959905+moisesdiazh@users.noreply.github.com>
2023-01-12 02:14:31 -0300

    configuraciones

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,6 +1,13 @@
-import * as firebase from 'firebase/app'
-import 'firebase/firestore';
-import 'firebase/auth';
+// import * as firebase from 'firebase/app'
+// import firebase from 'firebase/app'
+
+// import "firebase/auth"
+// import 'firebase/firestore';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 import { GoogleAuthProvider } from 'firebase/auth'
 
 
